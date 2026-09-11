91def38b7e05ca064c98a9133b12fa46a96ff7ba
ijalj3115-ctrl <ijalj3115@gmail.com>
2026-09-11 16:53:45 +0700

    Add files via upload

---
+++ 04_nilai_akhir.py
@@ -0,0 +1,19 @@
+print("PROGRAM NILAI AKHIR")
+
+nama = input("Nama: ")
+nilai_tugas = float(input("Nilai Tugas: "))
+nilai_uts = float(input("Nilai UTS: "))
+nilai_uas = float(input("Nilai UAS: "))
+
+BOBOT_TUGAS = 0.20
+BOBOT_UTS = 0.30
+BOBOT_UAS = 0.50
+
+nilai_akhir = (
+    nilai_tugas * BOBOT_TUGAS
+    + nilai_uts * BOBOT_UTS
+    + nilai_uas * BOBOT_UAS
+)
+
+print(f"\nNama : {nama}")
+print(f"Nilai akhir = {nilai_akhir:.2f}")
